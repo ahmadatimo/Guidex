d5c435dfab849074ff7c6a9d9df70aa96b2171b9
Maksat <maksatabrayew63@gmail.com>
2024-10-08 18:55:04 +0300

    Frontend file strucuture added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-center text-lg font-bold">
+        Be a man bro
+      </h1>
+    </div>
+  );
+};
+
+export default App;
