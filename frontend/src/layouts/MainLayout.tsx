359ff844f35289f0abe6ae4c520d72670c656550
Ahmed Hatem <ahmadatimo2002@gmail.com>
2024-10-28 09:58:16 +0300

    sidebar is capital

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -2,12 +2,12 @@ import { Outlet } from 'react-router-dom'
 // import NavBar from '../components/NavBar'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/ReactToastify.css";
-import Sidebar from '../components/SideBar';
+import SideBar from '../components/SideBar';
 
 const MainLayout = () => {
     return (
         <>
-            <Sidebar />
+            <SideBar /> 
             <Outlet />
             <ToastContainer/>
         </>
