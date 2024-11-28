40222d0a012033338575f7e74818a0291081c8d0
Maksat <maksatabrayew63@gmail.com>
2024-11-29 01:22:12 +0300

    ORM added

--- backend/main.py
+++ backend/main.py
@@ -1,63 +1,35 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel, EmailStr
-from typing import Generator
-from mysql.connector import connect, Error
-from dotenv import load_dotenv
-import os
+from typing import Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
-# Load environment variables
-load_dotenv()
-
-# Database configuration
-DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
-DATABASE_USER = os.getenv("DATABASE_USER", "root")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "guidex")
-
-# Create database connection
-def get_db_connection() -> Generator:
-    try:
-        connection = connect(
-            host=DATABASE_HOST,
-            port=DATABASE_PORT,
-            user=DATABASE_USER,
-            password=DATABASE_PASSWORD,
-            database=DATABASE_NAME
-        )
-        yield connection
-    finally:
-        connection.close()
-
-# FastAPI app
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
-# Pydantic Schema for User
-class UserCreate(BaseModel):
-    name: str
-    email: EmailStr
+class AppointmentBase(BaseModel):
+    user_id: int
+
+
+class UserBase(BaseModel):
+    user_email: EmailStr
     password: str
+    role:     str
 
-# API Endpoint to add a user
-@app.post("/users/", status_code=201)
-def create_user(user: UserCreate, db=Depends(get_db_connection)):
+
+def get_db():
+    db = SessionLocal()
     try:
-        # Check if user with the same email exists
-        check_user_query = "SELECT * FROM users WHERE email = %s"
-        with db.cursor(dictionary=True) as cursor:
-            cursor.execute(check_user_query, (user.email,))
-            existing_user = cursor.fetchone()
-            if existing_user:
-                raise HTTPException(status_code=400, detail="Email already registered")
+        yield db
+    finally:
+        db.close()
 
-        # Insert new user
-        insert_user_query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
-        with db.cursor() as cursor:
-            cursor.execute(insert_user_query, (user.name, user.email, user.password))
-            db.commit()
-            new_user_id = cursor.lastrowid
+db_dependency = Annotated[Session, Depends(get_db)]
 
-        return {"id": new_user_id, "name": user.name, "email": user.email}
 
-    except Error as e:
-        raise HTTPException(status_code=500, detail=f"Database error: {e}")
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.User(**user.model_dump())
+    db.add(db_user)
+    db.commit()
---
+++ backend/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Boolean, Integer, String, Column
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_email = Column(String(50), unique=True)
+    password = Column(String(50))
+    role= Column(String(20))
+
+
+
+class Appointment(Base):
+    __tablename__ = "appointments"
+
+    id = Column(Integer, primary_key= True, index=True)
+    user_id = Column(Integer)
+    
+    
+    
