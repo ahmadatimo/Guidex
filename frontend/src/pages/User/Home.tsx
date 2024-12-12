61abde23aabb31b406c2a2f9cfbf201c6d92d7a3
mefe SAK <mefesak007@gmail.com>
2024-12-12 23:05:23 +0300

    home feedbacklink fixed

--- frontend/src/pages/User/Home.tsx
+++ frontend/src/pages/User/Home.tsx
@@ -13,12 +13,13 @@ const Home: React.FC = () => {
       <Testimonials />
       <HowItWorks />
       <div className="text-center mt-6">
-        <Link to="visitor/feedback">
+        <Link to="/visitor/feedback">
           <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
             Leave Feedback
           </button>
         </Link>
       </div>
+
     </>
   );
 };
